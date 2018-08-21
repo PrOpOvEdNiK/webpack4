905eb25f00229c6dc1aa846bba4669349d128095
Sergey Buynitsky <coder8@web-axioma.ru>
2018-08-21 14:56:12 +0700

    Добавлен пример ES6 класса

--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,65 @@ import 'bootstrap';
 
 import * as f from "./modules/functions";
 
-const APP = 'WEBPACK4_BOILERPLATE: ';
+const APP_CONTAINER = '#app';
+const APP_NAME = 'WEBPACK4_BOILERPLATE: ';
 
 window.$ = window.jQuery = $;
 
-$(document).ready(function ($) {
-    if (!f.isMobile()) {
-        f.delay(500).then(
-            () => {
-                $('<div></div>', {
-                    class: 'main',
-                    text: APP + 'This is not a mobile!'
-                }).appendTo('#app');
-            }
-        );
+class App {
+    constructor(name, container) {
+        this.AppName = name;
+        this.AppContainer = container;
+        this.rowClass = 'main col-12';
     }
 
-    console.log($.fn.jquery);
+    hello() {
+        $('<div></div>', {
+            class: this.rowClass,
+            text: this.AppName + 'Hello, world!'
+        }).appendTo(this.AppContainer);
+    }
+
+    whichJquery() {
+        $('<div></div>', {
+            class: this.rowClass,
+            text: this.AppName + 'jQuery version is: ' + $.fn.jquery
+        }).appendTo(this.AppContainer);
+    }
+
+    isMobile() {
+        let text = '';
+        if (!f.isMobile()) {
+            text = this.AppName + 'This is not a mobile!';
+        } else {
+            text = this.AppName + 'This is a mobile!';
+        }
+        $('<div></div>', {
+            class: this.rowClass,
+            text: text
+        }).appendTo(this.AppContainer);
+    }
+}
+
+let APP = new App(APP_NAME, APP_CONTAINER);
+
+f.delay(1000).then(
+    () => {
+        APP.hello();
+    }
+);
+f.delay(2000).then(
+    () => {
+        APP.whichJquery();
+    }
+);
+f.delay(3000).then(
+    () => {
+        APP.isMobile();
+    }
+);
+
+$(document).ready(function ($) {
+
 });
 
